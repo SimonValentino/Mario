ad3bd849f758802f87e558567b27486d328f0bab
Simon Valentino <simontvalentino@gmail.com>
2023-05-09 08:22:10 -0400

    Started to render map.

--- core/src/com/svalentino/Screens/PlayScreen.java
+++ core/src/com/svalentino/Screens/PlayScreen.java
@@ -1,9 +1,14 @@
 package com.svalentino.Screens;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.svalentino.MarioGame;
@@ -19,11 +24,18 @@ public class PlayScreen implements Screen {
     private Viewport vport;
     private GameHud hud;
 
+    private TiledMap map;
+    private OrthogonalTiledMapRenderer renderer;
+
     public PlayScreen(MarioGame game) {
         this.game = game;
         this.camera = new OrthographicCamera();
         this.vport = new FitViewport(MarioGame.WIDTH, MarioGame.HEIGHT, camera);
         this.hud = new GameHud(game.batch);
+
+        this.map = new TmxMapLoader().load("MarioMap.tmx");
+        this.renderer = new OrthogonalTiledMapRenderer(map);
+        camera.position.set(vport.getWorldWidth() / 2, vport.getWorldHeight() / 2, 0);
     }
 
     @Override
@@ -38,6 +50,10 @@ public class PlayScreen implements Screen {
     @Override
     public void render(float delta) {
         clearScreen();
+
+        renderMap();
+        renderer.render();
+
         // setup where the batch will project to
         // getting the hud's camera
         game.batch.setProjectionMatrix(hud.stage.getCamera().combined);
@@ -75,5 +91,17 @@ public class PlayScreen implements Screen {
     private void clearScreen() {
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+    }
+
+    private void getInput() {
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT))
+            camera.position.x -= 1;
+        else if (Gdx.input.isKeyPressed(Input.Keys.RIGHT))
+            camera.position.x += 1;
+    } 
+
+    private void renderMap() {
+        getInput();
+        renderer.setView(camera);
     }
 }
