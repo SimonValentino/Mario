d327f9fccb7f31daa3eb1999bba3cf37fb002eb0
simonvalentino <simontvalentino@gmail.com>
2023-05-10 19:35:27 -0400

    Added object layers in tile map to BoxD2 World.

--- core/src/com/svalentino/Screens/PlayScreen.java
+++ core/src/com/svalentino/Screens/PlayScreen.java
@@ -5,10 +5,20 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.svalentino.MarioGame;
@@ -19,13 +29,25 @@ Actual rendering of the MarioGame is delegated to a screen.
 Any screen class should implement Screen.
  */
 public class PlayScreen implements Screen {
+    // Game
     private MarioGame game;
+
+    // Camera
     private OrthographicCamera camera;
     private Viewport vport;
+
+    // Hud
     private GameHud hud;
 
+    // Map
     private TiledMap map;
     private OrthogonalTiledMapRenderer renderer;
+
+    // Physics engine
+    World world;
+    Box2DDebugRenderer box2DRenderer;
+
+
 
     public PlayScreen(MarioGame game) {
         this.game = game;
@@ -37,6 +59,32 @@ public class PlayScreen implements Screen {
         this.renderer = new OrthogonalTiledMapRenderer(map);
         camera.position.set(vport.getWorldWidth() / 2, vport.getWorldHeight() / 2, 0);
         camera.update();
+
+        this.world = new World(new Vector2(0, 0), true);
+        this.box2DRenderer = new Box2DDebugRenderer();
+
+        Body body;
+
+        BodyDef bodyDef = new BodyDef();
+        FixtureDef fixtureDef = new FixtureDef();
+        PolygonShape shape = new PolygonShape();
+        Rectangle rect;
+
+        for (int i = 2; i <= 6; i++) {
+            for (RectangleMapObject obj : map.getLayers().get(i).getObjects().getByType(RectangleMapObject.class)) {
+                rect = obj.getRectangle();
+
+                // StaticBody means not effected by gravity, cant move, etc.
+                bodyDef.type = BodyDef.BodyType.StaticBody;
+                bodyDef.position.set(rect.x + rect.width / 2, rect.y + rect.height / 2);
+
+                shape.setAsBox(rect.width / 2, rect.height / 2);
+                fixtureDef.shape = shape;
+
+                body = world.createBody(bodyDef);
+                body.createFixture(fixtureDef);
+            }
+        }
     }
 
     @Override
@@ -54,6 +102,9 @@ public class PlayScreen implements Screen {
 
         renderMap(delta);
         renderer.render();
+
+        // render the Box2D blocks
+        box2DRenderer.render(world, camera.combined);
 
         // setup where the batch will project to
         // getting the hud's camera
@@ -99,11 +150,15 @@ public class PlayScreen implements Screen {
             camera.position.x -= 100 * delta;
         else if (Gdx.input.isKeyPressed(Input.Keys.RIGHT))
             camera.position.x += 100 * delta;
-    } 
+    }
 
     private void renderMap(float delta) {
         getInput(delta);
         camera.update();
         renderer.setView(camera);
+    }
+
+    private void setupPhysicsEngineBody() {
+
     }
 }
