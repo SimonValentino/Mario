ae045674113311884da0895953df42bc8ebad6f9
Simon Valentino <simontvalentino@gmail.com>
2023-05-15 12:04:46 -0400

    No mario friction.

--- core/src/com/svalentino/characters/Mario.java
+++ core/src/com/svalentino/characters/Mario.java
@@ -39,10 +39,7 @@ public class Mario extends Sprite implements Disposable {
         hitbox.setAsBox(marioWidth * MarioGame.SCALE, marioHeight * MarioGame.SCALE);
 
         fixtureDef.shape = hitbox;
-<<<<<<< HEAD
-=======
         // fixtureDef.friction = 0.7f;
->>>>>>> 1b380a0a7836cf50ff88f3d90881388e82181383
         mario.createFixture(fixtureDef);
     }
 
