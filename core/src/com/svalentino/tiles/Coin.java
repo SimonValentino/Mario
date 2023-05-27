8372fb0403f4c0e96aa5fd2a48bcb658cfe5fa16
simonvalentino <simontvalentino@gmail.com>
2023-05-27 14:40:47 -0400

    Changed goomba velocity.

--- core/src/com/svalentino/tiles/Coin.java
+++ core/src/com/svalentino/tiles/Coin.java
@@ -19,7 +19,7 @@ public class Coin extends PhysicalObject implements InteractableObject {
 
     @Override
     public void hitMarioHead() {
-        Gdx.app.log("Coin", "Collisions");
+        Gdx.app.log("Coin", "Collision");
         setCategory(MarioGame.DESTROYED_COL);
         getCell().setTile(null);
         GameHud.updateScore(200);
