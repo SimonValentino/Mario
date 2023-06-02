1e9237132655e8c473a294eb8c521cecb1ad00d2
marriojeyakumar <marriojeyakumar123@gmail.com>
2023-06-02 14:46:05 -0400

    BROKEN CODE

--- core/src/com/svalentino/SpriteManager.java
+++ core/src/com/svalentino/SpriteManager.java
@@ -13,7 +13,8 @@ public class SpriteManager extends Sprite {
     public SpriteManager () {
         atlas = new TextureAtlas(Gdx.files.internal("Downloads/Mario_and_Enemies.pack"));
         small_mario_stand = atlas.findRegion("small_mario_stand");
-    //    setBounds(500, 5, 16, 16);
+        setBounds(500, 5, 32, 100);
+        
     }
     public TextureRegion getSmall_mario_stand() {
         return small_mario_stand;
@@ -21,8 +22,5 @@ public class SpriteManager extends Sprite {
     public static TextureAtlas getAtlas() {
         return atlas;
     }
-    @Override
-    public void setPosition (float x, float y) {
-       // super.setPosition(x + atlas., y + region.offsetY);
-    }
+
 }
