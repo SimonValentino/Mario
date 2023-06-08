c17e2137f21c418563c875d815f68a4efb8382c3
simonvalentino <simontvalentino@gmail.com>
2023-06-08 12:51:43 -0400

    .

--- core/src/com/svalentino/characters/Mario.java
+++ core/src/com/svalentino/characters/Mario.java
@@ -55,7 +55,7 @@ public class Mario extends Sprite implements Disposable {
         frames.clear();
 
         for (int i = 4; i < 6; i++)
-            frames.add(new TextureRegion(getTexture(), i * 16, 0, 16, 16));
+            frames.add(new TextureRegion(getTexture(), i * 16, 0, 15, 16));
         marioJump = new Animation(0.1f, frames);
 
         this.isDead = false;
@@ -116,7 +116,7 @@ public class Mario extends Sprite implements Disposable {
             walkedOff = true;
         }
 
-        mario.setLinearVelocity(new Vector2(15f, -55f));
+        mario.setLinearVelocity(new Vector2(15f, -45f));
     }
 
     public void goThroughExitDoor() {
